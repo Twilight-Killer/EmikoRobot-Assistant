18a94e4fba281283956cd96313b56f2ece9ebbea
Moon Knight <103847581+Twilight-Killer@users.noreply.github.com>
2022-10-13 01:32:41 +0700

    Update inlinefuncs.py

--- EmikoRobot/utils/inlinefuncs.py
+++ EmikoRobot/utils/inlinefuncs.py
@@ -102,7 +102,7 @@ async def inline_help_func(__HELP__):
             input_message_content=InputTextMessageContent(
                 "**__Click A Button To Get Started.__**"
             ),
-            thumb_url="https://https://telegra.ph/file/cc528b1f6ed4b7acd3049.jpg",
+            thumb_url="https://telegra.ph/file/cc528b1f6ed4b7acd3049.jpg",
             reply_markup=buttons,
         ),
     ]
